6910d90eb936184454ca1d1b60ea615d0916eee5
Douglas Lopes <dwillian.lopes@gmail.com>
2018-12-28 22:40:21 -0200

    add update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -6,18 +6,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server'); 
 
-/*     db.collection('Todos').findOneAndUpdate({
-        _id: new ObjectID('5c267cd070b62a45a9c41335')
-    }, {
-        $set: {
-            completed: true
-        }
-    }, {
-        returnOriginal: false
-    }).then((result) => {
-        console.log(JSON.stringify(result, undefined, 2));
-    }) */
-
     db.collection('Users').findOneAndUpdate({
         _id: new ObjectID('5c26b78a1bd14dc54fb4dc72')
     }, {
