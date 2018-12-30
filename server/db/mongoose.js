3715dc928bf643f14920490cde91965c2c41fd5d
Douglas Lopes <dwillian.lopes@gmail.com>
2018-12-30 21:06:43 -0200

    adding todo/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const {ObjectID} = require('mongodb');
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose,
-    Schema: mongoose.Schema
+    Schema: mongoose.Schema,
+    ObjectID
 };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var mongoose = require('./db/mongoose');
+const { mongoose, ObjectID } = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo')
 
@@ -19,7 +19,7 @@ app.post('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
-})
+});
 
 app.get('/todos', (req, res) => {
    Todo.find().then((todos) => {
@@ -27,6 +27,23 @@ app.get('/todos', (req, res) => {
    }, (e) => {
         res.status(400).send(e);
    }) 
+})
+
+app.get('/todos/:id', (req, res) => {
+    const {id} = req.params;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('Id Inválido');
+    };
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            res.status(404).send('Nenhum Id Encontrado');
+        } else {
+            res.send({todo});
+        }
+    }, (e) => {
+        res.status(400).send('Bad request');
+    })
 })
 
 app.listen(3000, () => {
