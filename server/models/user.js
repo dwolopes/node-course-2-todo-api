5fa05203378871b12ad98149a558c4289c1da089
Douglas Lopes <dwillian.lopes@gmail.com>
2019-01-02 01:04:32 -0200

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 var { mongoose, Schema } = require('../db/mongoose');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 const validateEmail = function(email) {
@@ -34,6 +35,20 @@ let user = new Schema({
             required: true
         }
     }]
+});
+
+user.pre('save', function(next) {
+    var user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
 });
 
 user.methods.toJSON = function () {
@@ -70,6 +85,27 @@ user.statics.findByToken =  function (token) {
         '_id': decoded._id,
         'tokens.token': token,
         'tokens.access': 'auth'
+    })
+}
+
+user.statics.findByCredentials =  function ({email, password}) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            })
+        })
+
     })
 }
 
